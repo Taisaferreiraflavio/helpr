d5425f0bbc4e28dbdae933392293901da093548c
Taisaferreiraflavio <taisaferreiraflavio@gmail.com>
2022-06-08 07:35:12 -0300

    Criando Rest Controller

--- src/main/java/com/api/helpr/repositories/TecnicoRepository.java
+++ src/main/java/com/api/helpr/repositories/TecnicoRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.helpr.domain.Tecnico;
 
 public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
+	
+	
+	
 
 }
