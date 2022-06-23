ad8cfc6a737934527e65058151dccdccd0af103f
Taisaferreiraflavio <taisaferreiraflavio@gmail.com>
2022-06-23 18:30:46 -0300

    tg_log_chamado_status

---
+++ MySQL/script_helpr/tg_log_chamado_status.sql
@@ -0,0 +1,14 @@
+use helpr_hyrsa;
+
+select * from chamado;
+
+DELIMITER //
+CREATE TRIGGER tg_log_status_chamado BEFORE UPDATE ON chamado
+FOR EACH ROW
+BEGIN
+	IF(OLD.status <> NEW.status) THEN
+    INSERT INTO log_chamado_status(data_alteracao, dado_alterado, dado_novo, dev_correcao)
+    VALUES(NOW(), OLD.status, NEW.status, NULL);
+    END IF;
+    END //
+    DELIMITER ;
